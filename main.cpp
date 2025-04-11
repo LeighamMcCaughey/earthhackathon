b9cb4e2dbdac130831b6157435a00bf5d5cbd4d0
HoguerV <vieyrahoguer4@gmail.com>
2025-04-11 23:17:39 +0000

    e

--- main.cpp
+++ main.cpp
@@ -14,9 +14,23 @@ int main()
     std::string color;
     std::cin >> color;
     Character player(name, color, age);
-    std::cout << "welcome to the world" << player.getName() << std::endl;
+    std::cout << "welcome to the world " << player.getName() << std::endl;
 
-    
+    bool failure = false;
+    while (failure != true)
+    {
+
+    }
+    if (failure == true)
+    {
+        std::cout << "You died! Try again!";
+    }
+    else
+    {
+        std::cout << "Congratulations.,!";
+    }
+
+
 
 }
 
